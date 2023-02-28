c137d2dc37cd54b5f433cacaaa375b3bb851160f
Bill Brown <bill.a.brown90@gmail.com>
2023-02-27 17:07:05 -0800

    learning fetch

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
+const jsonArray = []
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
+  return fetch("https://anapioficeandfire.com/api/books")
+    .then((resp)=>resp.json())
+    .then((json) => renderBooks(json)) 
 }
 
+//function provided in lab
 function renderBooks(books) {
   const main = document.querySelector('main');
   books.forEach(book => {
@@ -15,3 +19,5 @@ function renderBooks(books) {
 document.addEventListener('DOMContentLoaded', function() {
   fetchBooks();
 });
+
+
